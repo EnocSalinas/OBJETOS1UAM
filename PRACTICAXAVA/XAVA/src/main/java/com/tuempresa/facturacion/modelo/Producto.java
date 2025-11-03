97ecfb2c25c113829c84d43855bfe16ea057c46e
EnocSalinas <wenocms@uamv.edu.ni>
2025-11-03 13:49:09 -0600

    Subo carpeta PRACTICAXAVA

---
+++ PRACTICAXAVA/XAVA/src/main/java/com/tuempresa/facturacion/modelo/Producto.java
@@ -0,0 +1,30 @@
+package com.tuempresa.facturacion.modelo;
+
+import javax.persistence.*;
+import org.openxava.annotations.*;
+import lombok.*;
+import java.math.BigDecimal;
+
+@Entity @Getter @Setter
+public class Producto {
+
+    @Id @Column(length=9)
+    int numero;
+
+    @Column(length=50) @Required
+    String descripcion;
+
+    @Money
+    BigDecimal precio;
+
+    @Files
+    @Column(length=32)
+    String fotos;
+
+    @TextArea
+    String observaciones;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = true)
+    @DescriptionsList
+    Categoria categoria;
+}
